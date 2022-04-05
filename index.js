d7998f22e1c1abb37599daef3b7f6e9932a70281
Eduard Tymoshuk <edward_t@mbp-edward.home>
2022-04-05 19:17:39 +0200

    add localhost to cors settings

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
   });
 
 //Route middlewares
