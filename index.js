02868568d8934b7dbd7768f99939988e29198f33
Eduard Tymoshuk <edward_t@mbp-edward.home>
2022-04-09 11:41:15 +0200

    cors adresses update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501")
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501", "http://eduardtymoshuk.epizy.com/")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
   });
